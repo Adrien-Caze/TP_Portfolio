6f1a5f8ad3c2588c80e6bff01abe041f88b1f7a0
Adrien <adrien.caze77@gmail.com>
2026-09-11 08:20:33 +0200

    pelican content -s publishconf.py

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,6 @@ from pelicanconf import *
 
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://github.com/Adrien-Caze/TP_Portfolio"
+SITEURL = "https://adrien-caze.github.io/TP_Portfolio/"
 RELATIVE_URLS = False
 DELETE_OUTPUT_DIRECTORY = True
